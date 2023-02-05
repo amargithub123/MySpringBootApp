712da6b77b9f33ba7ae6d5749a0700a5da586076
amargithub123 <amarbhairat25@gmail.com>
2023-02-05 13:44:44 +0530

    HIS-200:changes

--- src/main/java/com/ab/springboot/SpringBootAppApplication.java
+++ src/main/java/com/ab/springboot/SpringBootAppApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAppApplication {
 
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
-
+	
+	//HIS-200 related changes
+	public void doProcess(){
+		//Logic go here
+	}
 }
